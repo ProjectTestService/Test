dcf084597ea1c920140a3271158d5b18ef7a5b26
ChinasoftMan <laiqiang@chinasoftinc.com>
2015-03-18 13:04:04 +0800

    first commit
    Signed-off-by: ChinasoftMan<laiqiang@chinasoftinc.com>

---
+++ src/test/HelloPts.java
@@ -0,0 +1,22 @@
+/**
+ * test package
+ */
+package test;
+
+/**
+ * Hello pts class
+ * @author l0000014610
+ *
+ */
+public class HelloPts {
+
+	/**
+	 * Program main function
+	 * @param args
+	 */
+    public static void main(String[] args) {
+    	// TODO Auto-generated method stub
+    	System.out.println("Hello PTS.");
+    }
+
+}
